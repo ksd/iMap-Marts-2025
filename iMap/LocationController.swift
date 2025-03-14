3611ceff2a3442005aa90d9ce7bdaacb5aa4b1d1
Kaj Schermer Didriksen <kdidriksen@me.com>
2025-03-14 10:15:38 +0100

    Nu med UserLocation 😃

---
+++ iMap/LocationController.swift
@@ -0,0 +1,8 @@
+//
+//  LocationController.swift
+//  iMap
+//
+//  Created by ksd on 14/03/2025.
+//
+
+import Foundation
---
+++ iMap/LocationsCoordinates.swift
@@ -0,0 +1,8 @@
+//
+//  LocationsCoordinates.swift
+//  iMap
+//
+//  Created by ksd on 11/03/2025.
+//
+
+import Foundation
